91b4269e68f235b35997ae045bdf1d4d106a5340
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-15 12:35:13 +0900

    step 21, const inputTitle, const inputBody

--- src/main/java/com/ll/sb20231114/ArticleController.java
+++ src/main/java/com/ll/sb20231114/ArticleController.java
@@ -6,7 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ArticleController {
