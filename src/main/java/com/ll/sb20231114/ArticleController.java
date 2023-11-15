12531f2e92429e6c83c7711d859980a492a2a6af
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-15 09:45:59 +0900

    ArticleController mission

--- src/main/java/com/ll/sb20231114/ArticleController.java
+++ src/main/java/com/ll/sb20231114/ArticleController.java
@@ -6,66 +6,68 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * ArticleController 부분 다 지우고 다시 만들어보세요. 35분까지
+ * 요청
+ * GET /article/write
+ * GET /article/doWrite?title=제목&body=내용
+ * GET /article/getLastArticle
+ * GET /article/getArticles
+ *
+ * 미션 수행하는데 조금 힘들었다 ㅠ 복습하자
+ */
 @Controller
 public class ArticleController {
-    // 장기 기억하기 위해서 밖으로 뺐다.
-    private List<Article> articles = new ArrayList<>(); // 여러개 저장하기 위해서
 
-    // 액션 메서드 만들기
-    // 게시글 작성
+    List<Article> articles = new ArrayList<>();
+
+    // 글 작성 버튼
     @GetMapping("/article/write")
-    String showWrite() {
+    String articleWrite() {
         return "article/write";
     }
 
-    // 글쓰기 버튼 누른 후에 저장
+    // 글 작성 버튼 누른 후 저장
     @GetMapping("/article/doWrite")
     @ResponseBody
-    RsData doWrite(
+    RsData<Article> articleDoWrite(
             String title,
             String body
     ) {
-        // 객체 만들기
+        // 객체 생성
         Article article = new Article(articles.size() + 1, title, body);
+        // 리스트에 저장
         articles.add(article);
 
         // 결과
-        // Article 버전의 RsData 객체
         RsData<Article> rs = new RsData<>(
                 "S-1",
-                "%d번 게시물이 작성되었습니다.".formatted(article.getId()),
+                "%d번 게시글이 저장되었습니다.".formatted(article.getId()),
                 article
         );
-
-        String resultCode = rs.getResultCode();
-        String msg = rs.getMsg();
-        Article _article = rs.getData(); // 형변환 안해도 된다.
 
         return rs;
     }
 
-    // 가장 마지막에 올라온 게시글 알려준다.
+    // 마지막 글 가져오기
     @GetMapping("/article/getLastArticle")
-    @ResponseBody
     Article getLastArticle() {
         return articles.getLast();
     }
 
-    // 모든 게시글 불러온다.
+    // 모든 글 가져오기
     @GetMapping("/article/getArticles")
-    @ResponseBody
     List<Article> getArticles() {
         return articles;
     }
 }
 
-// 보고서 양식 - 다른데서도 쓴다.
-// 어떤 클래스 미완성인게 좋다.
 @AllArgsConstructor
 @Getter
-class RsData<T> { // ResultData라는 뜻
+class RsData<T> {
     private String resultCode;
     private String msg;
     private T data;
