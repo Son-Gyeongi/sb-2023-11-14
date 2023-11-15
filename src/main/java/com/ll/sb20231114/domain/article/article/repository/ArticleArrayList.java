17887acba2c4baebd1d67c3491e6f0f4e8bd67ef
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-15 23:47:20 +0900

    step 42, ArticleArrayList

---
+++ src/main/java/com/ll/sb20231114/domain/article/article/repository/ArticleArrayList.java
@@ -0,0 +1,9 @@
+package com.ll.sb20231114.domain.article.article.repository;
+
+import com.ll.sb20231114.domain.article.article.entity.Article;
+
+import java.util.ArrayList;
+
+public class ArticleArrayList extends ArrayList<Article> {
+    // ArrayList<Article>의 기능 모두 받음
+}
--- src/main/java/com/ll/sb20231114/domain/article/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/sb20231114/domain/article/article/repository/ArticleRepository.java
@@ -1,17 +1,20 @@
 package com.ll.sb20231114.domain.article.article.repository;
 
 import com.ll.sb20231114.domain.article.article.entity.Article;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository // 빈에 등록
+@RequiredArgsConstructor
 public class ArticleRepository {
 
     // 장기 기억하기 위해서 밖으로 뺐다.
-    private List<Article> articles = new ArrayList<>(); // 여러개 저장하기 위해서
+//    private List<Article> articles = new ArrayList<>(); // 여러개 저장하기 위해서
 
+    // 원래 만들어진 ArrayList에도 생성자 주입 해보기
+    private final ArticleArrayList articles;
 
     public Article save(Article article) {
         // id값 추가하기
