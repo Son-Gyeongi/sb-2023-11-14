7be2ec6b1bcea5a483b10fda918dd8bf1c3daa23
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-15 23:30:45 +0900

    step 34, ArticleRepository

---
+++ src/main/java/com/ll/sb20231114/domain/article/article/repository/ArticleRepository.java
@@ -0,0 +1,32 @@
+package com.ll.sb20231114.domain.article.article.repository;
+
+import com.ll.sb20231114.domain.article.article.entity.Article;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArticleRepository {
+
+    // 장기 기억하기 위해서 밖으로 뺐다.
+    private List<Article> articles = new ArrayList<>(); // 여러개 저장하기 위해서
+
+
+    public Article save(Article article) {
+        // id값 추가하기
+        if (article.getId() == null) { // null을 활용하기 위해서 id필드 Long타입으로 변경
+            article.setId(articles.size() + 1L);
+        }
+
+        articles.add(article);
+
+        return article;
+    }
+
+    public Article findLastArticle() {
+        return articles.getLast();
+    }
+
+    public List<Article> findAll() {
+        return articles;
+    }
+}
--- src/main/java/com/ll/sb20231114/domain/article/article/service/ArticleService.java
+++ src/main/java/com/ll/sb20231114/domain/article/article/service/ArticleService.java
@@ -1,31 +1,32 @@
 package com.ll.sb20231114.domain.article.article.service;
 
 import com.ll.sb20231114.domain.article.article.entity.Article;
+import com.ll.sb20231114.domain.article.article.repository.ArticleRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ArticleService {
 
-    // 장기 기억하기 위해서 밖으로 뺐다.
-    private List<Article> articles = new ArrayList<>(); // 여러개 저장하기 위해서
+    private final ArticleRepository articleRepository = new ArticleRepository();
 
     // 게시글 작성
     public Article write(String title, String body) {
         // 객체 만들기
-        Article article = new Article(articles.size() + 1, title, body);
-        articles.add(article);
+        Article article = new Article(title, body);
+
+        articleRepository.save(article);
 
         return article;
     }
 
     // 마지막 게시글 조회
     public Article findLastArticle() {
-        return articles.getLast();
+        return articleRepository.findLastArticle();
     }
 
     // 모든 게시글 조회
     public List<Article> findAll() {
-        return articles;
+        return articleRepository.findAll();
     }
 }
