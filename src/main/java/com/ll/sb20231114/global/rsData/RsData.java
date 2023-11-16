3bf657baf0a539bf9c0316e3cf72f20b975548e9
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-16 13:01:05 +0900

    step 51, case

--- src/main/java/com/ll/sb20231114/global/rsData/RsData.java
+++ src/main/java/com/ll/sb20231114/global/rsData/RsData.java
@@ -2,13 +2,15 @@ package com.ll.sb20231114.global.rsData;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 // 보고서 양식 - 다른데서도 쓴다.
 // 어떤 클래스 미완성인게 좋다.
 @AllArgsConstructor
+@RequiredArgsConstructor
 @Getter
 public class RsData<T> { // ResultData라는 뜻
-    private String resultCode;
-    private String msg;
+    private final String resultCode;
+    private final String msg;
     private T data;
 }
