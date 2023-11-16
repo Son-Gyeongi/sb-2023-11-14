7b94e1a3943d277c0530e6134413df32d3888483
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-16 23:45:13 +0900

    step 66, test data

--- src/main/java/com/ll/sb20231114/domain/article/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/sb20231114/domain/article/article/repository/ArticleRepository.java
@@ -4,6 +4,7 @@ import com.ll.sb20231114.domain.article.article.entity.Article;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository // 빈에 등록
@@ -14,7 +15,11 @@ public class ArticleRepository {
 //    private List<Article> articles = new ArrayList<>(); // 여러개 저장하기 위해서
 
     // AppConfig 만들어서 빈 관리하기, AppConfig에 있는 메서드명이랑 같은 것이 매칭된다.
-    private final List<Article> articles;
+    private final List<Article> articles = new ArrayList<>() {{
+        add(new Article(1L, "제목 1", "내용 1"));
+        add(new Article(2L, "제목 2", "내용 2"));
+        add(new Article(3L, "제목 3", "내용 3"));
+    }};
 
     public Article save(Article article) {
         // id값 추가하기
--- src/main/java/com/ll/sb20231114/global/app/AppConfig.java
+++ src/main/java/com/ll/sb20231114/global/app/AppConfig.java
@@ -1,25 +1,7 @@
 package com.ll.sb20231114.global.app;
 
-import com.ll.sb20231114.domain.article.article.entity.Article;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 
 @Configuration // 설정 파일
 public class AppConfig {
-
-    @Bean
-        // 빈 등록
-    List<Article> articles2() {
-        return new LinkedList<>();
-    }
-
-    // 빈 한개 더 등록해보기
-    @Bean
-    List<Article> articles() {
-        return new ArrayList<>();
-    }
 }
