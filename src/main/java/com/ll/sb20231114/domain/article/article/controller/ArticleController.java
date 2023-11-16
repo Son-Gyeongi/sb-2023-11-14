5040bf0bc91c5563fda7ecd0e0044453e88a012d
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-17 00:02:50 +0900

    step 72

--- src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor // 생성자 주입, final 붙은 필드만 생성
@@ -37,9 +36,7 @@ public class ArticleController {
     @GetMapping("/article/detail/{id}")
     @ResponseBody
     String showDetail(@PathVariable long id) {
-        Optional<Article> opArticle = articleService.findById(id);
-
-        Article article = opArticle.get(); // Optional이면 0~1개 값이 온다. null이면 프로그램 뻗는다.
+        Article article = articleService.findById(id).get(); // Optional이면 0~1개 값이 온다. null이면 프로그램 뻗는다.
 
         return "article/detail";
     }
