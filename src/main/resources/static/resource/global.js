ce5ed46616c343932f54f1f007d2799bfbc05b12
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-19 23:52:29 +0900

    notice javascript/html

--- src/main/resources/static/resource/global.js
+++ src/main/resources/static/resource/global.js
@@ -24,6 +24,7 @@ function toastNotice(msg) {
     toastr["success"](msg, "성공");
 }
 
+// 자바스크립트에서 현재 페이지 url 뒤에 파라미터를 객체 형태로 얻기
 function getQueryParams() {
     const params = new URLSearchParams(window.location.search);
     const paramsObj = {};
