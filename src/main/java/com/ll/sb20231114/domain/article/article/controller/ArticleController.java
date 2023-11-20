2e0d5460004fbc69bf2a9bd2e5940c2ce7298ae1
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-21 01:49:51 +0900

    step 16, remove httpServletRequest

--- src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
@@ -72,7 +72,9 @@ public class ArticleController {
 
     // 게시글 작성
     @GetMapping("/article/write")
-    String showWrite(HttpServletRequest req) {
+    String showWrite() {
+        HttpServletRequest req = rq.getReq();
+
         long loginedMemberId = Optional
                 .ofNullable(req.getSession().getAttribute("loginedMemberId"))
                 .map(id -> (long) id)
@@ -81,9 +83,6 @@ public class ArticleController {
         if (loginedMemberId > 0) {
             Member loginedMember = memberService.findById(loginedMemberId).get();
             req.setAttribute("loginedMember", loginedMember);
-            // 위와 같은 코드 model.addAttribute("loginedMember", loginedMember);
-            // HttpServletRequest를 안 쓰려고 Model을 쓴거 였는데 지금은 HttpServletRequest가 있으므로
-            // 하나로 합치자
         }
 
         return "article/article/write";
