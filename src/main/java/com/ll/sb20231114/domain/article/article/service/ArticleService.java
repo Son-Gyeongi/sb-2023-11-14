3bb248762426f10da301685b8f0721bd5424a0e9
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-22 11:41:57 +0900

    step 49, ROLE_ADMIN delete

--- src/main/java/com/ll/sb20231114/domain/article/article/service/ArticleService.java
+++ src/main/java/com/ll/sb20231114/domain/article/article/service/ArticleService.java
@@ -54,6 +54,11 @@ public class ArticleService {
     }
 
     public boolean canDelete(Member actor, Article article) {
-        return canModify(actor, article);
+        // 관리자의 경우 사용자 게시물 삭제 허용
+        if (actor.isAdmin()) return true;
+
+        if (actor == null) return false;
+
+        return article.getAuthor().equals(actor);
     }
 }
