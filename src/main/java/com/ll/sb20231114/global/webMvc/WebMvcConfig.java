a86b7e67e0484daf69f48ea85e7768b3650459aa
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-22 11:23:01 +0900

    step 44, NeedToLogoutInterceptor

--- src/main/java/com/ll/sb20231114/global/webMvc/WebMvcConfig.java
+++ src/main/java/com/ll/sb20231114/global/webMvc/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.ll.sb20231114.global.webMvc;
 
 import com.ll.sb20231114.global.interceptor.NeedToAdminInterceptor;
 import com.ll.sb20231114.global.interceptor.NeedToLoginInterceptor;
+import com.ll.sb20231114.global.interceptor.NeedToLogoutInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -10,16 +11,25 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
+    private final NeedToLogoutInterceptor needToLogoutInterceptor;
     private final NeedToLoginInterceptor needToLoginInterceptor;
     private final NeedToAdminInterceptor needToAdminInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        // 로그아웃 후에 접속 가능
+        registry.addInterceptor(needToLogoutInterceptor)
+                .addPathPatterns("/member/login")
+                .addPathPatterns("/member/join")
+                .addPathPatterns("/member/findUsername")
+                .addPathPatterns("/member/findPassword");
+        // 로그인 후에 접속 가능
         registry.addInterceptor(needToLoginInterceptor)
                 .addPathPatterns("/adm/**")
                 .addPathPatterns("/article/write")
                 .addPathPatterns("/article/modify/**")
                 .addPathPatterns("/article/delete/**");
+        // 관리자일 경우 접속 가능
         registry.addInterceptor(needToAdminInterceptor)
                 .addPathPatterns("/adm/**");
     }
