13f76438360055d32aab4d96efc71b628abae28b
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-22 13:10:15 +0900

    step 14, Rq / User

--- src/main/java/com/ll/sb20231114/global/rq/Rq.java
+++ src/main/java/com/ll/sb20231114/global/rq/Rq.java
@@ -2,30 +2,41 @@ package com.ll.sb20231114.global.rq;
 
 import com.ll.sb20231114.domain.member.member.entity.Member;
 import com.ll.sb20231114.domain.member.member.service.MemberService;
+import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.Optional;
 
 @RequestScope
 @Component
 @Getter
+@RequiredArgsConstructor
 public class Rq {
     private final HttpServletRequest req;
     private final HttpServletResponse resp;
     private final MemberService memberService;
+    private User user;
     private Member member;
     // 멤버는 장기기억 했으면 좋겠다. final 안 쓰는 이유 member 생성된 후에 값을 넣어야 하므로
 
-    public Rq(HttpServletRequest req, HttpServletResponse resp, MemberService memberService) {
-        this.req = req;
-        this.resp = resp;
-        this.memberService = memberService;
+    // 생성자가 실행된 후에 @PostConstructor가 실행됨
+    @PostConstruct
+    public void init() {
+        // 현재 로그인한 회원의 인증정보를 가져옴
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication.getPrincipal() instanceof User) {
+            this.user = (User) authentication.getPrincipal();
+        }
     }
 
     public String redirect(String path, String msg) {
@@ -35,15 +46,12 @@ public class Rq {
     }
 
     // 로그인 된 사용자 id 가져오기, 없으면 0 반환
-    private long getMemberId() {
-        return Optional
-                .ofNullable(req.getSession().getAttribute("loginedMemberId"))
-                .map(id -> (long) id)
-                .orElse(0L);
+    private String getMemberUsername() {
+        return user.getUsername();
     }
 
     public boolean isLogined() {
-        return getMemberId() > 0;
+        return user != null;
     }
 
     // 로그인 된 멤버 찾기
@@ -54,7 +62,7 @@ public class Rq {
         }
 
         if (member == null)
-            member = memberService.findById(getMemberId()).get();
+            member = memberService.findByUsername(getMemberUsername()).get();
 
         return member;
     }
@@ -76,6 +84,8 @@ public class Rq {
 
     // 회원 권한 확인
     public boolean isAdmin() {
-        return getMember().isAdmin();
+        return user.getAuthorities()
+                .stream()
+                .anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"));
     }
 }
