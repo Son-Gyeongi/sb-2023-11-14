33f1d866c76d18294ffecfc46fdaebc3ec4bc2ec
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-25 01:58:15 +0900

    step 54, 컨트롤러에서 historyBack 을 하지 않기 때문에, 쓸데없는 코드인 return rq.historyBack(joinRs.getMsg()); 을 제거

--- src/main/java/com/ll/sb20231114/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/sb20231114/domain/member/member/controller/MemberController.java
@@ -48,10 +48,6 @@ public class MemberController {
         // 컨트롤러에서 예외를 처리하지 않음, 결국 요청처리흐름이 중단됨
         RsData<Member> joinRs = memberService.join(joinForm.username, joinForm.password);
 
-        if (joinRs.isFail()) {
-            return rq.historyBack(joinRs.getMsg());
-        }
-
         // 멤버 가입 GET 으로 이동
         return rq.redirect("/member/login", joinRs.getMsg());
     }
