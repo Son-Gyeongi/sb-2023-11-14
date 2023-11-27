44cfe01145929ef7c9631911e75daede0c47e4ee
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-27 23:15:55 +0900

    step 9, Attr 클래스에 LocalDatetime createDate 칼럼을 추가, format_sql 옵션 켜서 실제로 실행되는 SQL 출력

--- src/main/java/com/ll/sb20231114/domain/base/attr/entity/Attr.java
+++ src/main/java/com/ll/sb20231114/domain/base/attr/entity/Attr.java
@@ -5,10 +5,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.time.LocalDateTime;
+
 @Entity
 public class Attr {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private LocalDateTime createDate;
     private String name;
 }
