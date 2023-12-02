6d2dc72d94a16cefd0ebc899aca1c006d7e4d293
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-03 02:09:56 +0900

    122강, 서비스가 RsData〈?〉 형태로 응답을 하면 호출자가 다양한 정보를 한번에 얻을 수 있어서 좋습니다.

--- src/main/java/com/ll/sb20231114/global/rsData/RsData.java
+++ src/main/java/com/ll/sb20231114/global/rsData/RsData.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 
 // 보고서 양식 - 다른데서도 쓴다.
 // 어떤 클래스 미완성인게 좋다.
+// 서비스가 RsData〈?〉 형태로 응답을 하면 호출자가 다양한 정보를 한번에 얻을 수 있어서 좋습니다.
 @AllArgsConstructor
 @RequiredArgsConstructor
 @Getter
@@ -15,7 +16,7 @@ public class RsData<T> { // ResultData라는 뜻
     private T data;
 
     public boolean isSuccess() {
-        return resultCode.startsWith("S-");
+        return resultCode.startsWith("S-"); // resultCode가 S로 시작하면 성공이다.
     }
 
     public boolean isFail() {
